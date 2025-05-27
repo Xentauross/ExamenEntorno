4edacffc686c14e42b5af4144c0a8678acfbe4d5
Xentauross <vicentepeinadoalcedo@gmail.com>
2025-05-27 09:45:41 +0200

    version 1.0_B

---
+++ prueba_escrita_B/src/prueba_escrita_B/menu.java
@@ -0,0 +1,12 @@
+package prueba_escrita_B;
+
+public class menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("version 1.0_B");
+
+	}
+
+}
